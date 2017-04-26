65ded6fc1d94a0dbd5c6ca2e8f1b9da5e7c3c6fb
Rob Dyke <robdyke@gmail.com>
2017-04-26 22:58:25 +0100

    Set xfreerdp opts and squash output
    xfreerdp options - /rfx +fonts +aero +menu-anims +compression +clipboard +window-drag
    sqiash output - > /dev/null 2>&1 &
    
     Changes to be committed:
    	modified:   notepad.sh

--- packages/nhsbuntu boxes/notepad.sh
+++ packages/nhsbuntu boxes/notepad.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-xfreerdp /cert-ignore /v:127.0.0.1 /u:vagrant /p:vagrant "/app:C:\Windows\notepad.exe" &
+xfreerdp /cert-ignore /v:127.0.0.1 /u:vagrant /p:vagrant "/app:C:\Windows\notepad.exe" /rfx +fonts +aero +menu-anims +compression +clipboard +window-drag > /dev/null 2>&1 &
